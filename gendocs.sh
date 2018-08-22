cd45fc839a91de1b183577cf81c33a543a73a35d
Roger Pence <roger.pence@gmail.com>
2018-08-21 23:29:51 -0500

    add control reference

--- gendocs.sh
+++ gendocs.sh
@@ -27,6 +27,9 @@ do
 		python ..\\..\\prep-for-classic-pycco.py $f
 		pycco $f.annotated -d ..\\docs\\classic -l javascript 
 		rm $f.annotated		
+		
+		python ..\\..\\..extract-props-from-classic.py $f
+		mv $f.controls.txt ..//docs//classic 
     else
 	    echo "File $f does not exist"
 	fi	
